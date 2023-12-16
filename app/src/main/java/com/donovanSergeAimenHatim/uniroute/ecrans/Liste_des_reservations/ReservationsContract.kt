54f82ebf46740b62cc424c5d97c575b8f3df1c10
hatim <hhatimi@dept-info.crosemont.quebec>
2023-12-15 21:30:54 -0500

    implementation de l'interface

--- app/src/main/java/com/donovanSergeAimenHatim/uniroute/ecrans/Liste_des_reservations/ReservationsContract.kt
+++ app/src/main/java/com/donovanSergeAimenHatim/uniroute/ecrans/Liste_des_reservations/ReservationsContract.kt
@@ -1,4 +1,9 @@
 package com.donovanSergeAimenHatim.uniroute.ecrans.Liste_des_reservations
 
+import com.donovanSergeAimenHatim.uniroute.ecrans.listTrajets.ReservationsModele
+
 interface ReservationsContract {
+    interface View {
+        fun displayReservations(reservationsModele: List<ReservationsModele.Reservation>)
+    }
 }
